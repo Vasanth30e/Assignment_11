4ef57c529689857745ac17d55ebe25ee3e0ed9ce
unknown <vasanthgamer2000@gmail.com>
2023-07-20 15:22:41 +0530

    IDisposable

---
+++ LargeDataCollection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_11
+{
+    public class LargeDataCollection : IDisposable
+    {
+
+        public int[] data;
+        public int Length;
+        public bool disposed = false;
+
+        public LargeDataCollection(params int[] initialData) 
+        {
+            data = initialData;
+        }
+
+        public void Add(int item)
+        {
+            int currentIndex = data.Length;
+            Array.Resize(ref data, currentIndex + 1);
+            data[currentIndex] = item;
+        }
+
+        public void Remove(int item)
+        {
+            int index = Array.IndexOf(data, item);
+            if(index >= 0)
+            {
+                for(int i = index; i < data.Length - 1; i++) 
+                {
+                    data[i] = data[i + 1];
+                }
+                Array.Resize(ref data,data.Length - 1);
+            }
+        }
+
+        public int GetElement(int index)
+        {
+            if(index >= 0 && index < data.Length)
+            {
+                return data[index];
+            }
+            throw new IndexOutOfRangeException("Index is out of Range");
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+
+                }
+                data = null;
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~LargeDataCollection() 
+        { 
+            Dispose(false); 
+        }
+
+        public void DisplayCllections()
+        {
+            Console.WriteLine("LargeDataCollction:");
+            foreach(int item in data)
+            {
+                Console.WriteLine(item+ "");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Console.WriteLine("Enter initial data for the LargeDataCollection separated by commas:");
+            string inputData = Console.ReadLine();
+            string[] dataItems = inputData.Split(',');
+            int[] initialData = Array.ConvertAll(dataItems, int.Parse);
+
+            LargeDataCollection largeDataCollection = new LargeDataCollection(initialData);
+            try
+            {
+                largeDataCollection.DisplayCllections();
+
+                Console.WriteLine("\nEnter additional elements to add to the collection separated by commas:");
+                string inputAdd = Console.ReadLine();
+                string[] addItems = inputAdd.Split(',');
+                foreach (string item in addItems)
+                {
+                    if (int.TryParse(item, out int numToAdd))
+                    {
+                        largeDataCollection.Add(numToAdd);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input: '{0}'. Skipping this element.", item);
+                    }
+                }
+
+                Console.WriteLine("\nEnter elements to remove from the collection separated by commas:");
+                string inputRemove = Console.ReadLine();
+                string[] removeItems = inputRemove.Split(',');
+                foreach (string item in removeItems)
+                {
+                    if (int.TryParse(item, out int numToRemove))
+                    {
+                        largeDataCollection.Remove(numToRemove);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input: '{0}'. Skipping this element.", item);
+                    }
+                }
+
+                Console.WriteLine("\nLargeDataCollection after removing elements:");
+                largeDataCollection.DisplayCllections();
+
+                Console.WriteLine("\nEnter index to access an element from the collection:");
+                if (int.TryParse(Console.ReadLine(), out int indexToAccess))
+                {
+                    try
+                    {
+                        int element = largeDataCollection.GetElement(indexToAccess);
+                        Console.WriteLine("Element at index {0}: {1}", indexToAccess, element);
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("Index is out of range.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for index. Please enter a valid integer.");
+                }
+            }
+            finally
+            {
+                // Explicitly call Dispose to release resources and free memory.
+                largeDataCollection.Dispose();
+                Console.WriteLine("Collections has been disposed") ;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
